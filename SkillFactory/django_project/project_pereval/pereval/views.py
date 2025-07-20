9ff6b5807fc5c9394f9be605ac2dd2314ac9c9bb
Dmitry-K-off <dimon1734371@gmail.com>
2025-07-21 04:26:44 +0500

    Спринт 1
    Реализован класс взаимодействия с базой данных.

--- SkillFactory/django_project/project_pereval/pereval/views.py
+++ SkillFactory/django_project/project_pereval/pereval/views.py
@@ -1,10 +1,10 @@
-# views.py
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
+
 from .serializers import PerevalSerializer
+from .services.submit_data_service import SubmitDataService
 
 
 @extend_schema(
@@ -12,12 +12,25 @@ from .serializers import PerevalSerializer
     responses={201: PerevalSerializer},
     tags=['tasks'],
     summary='Добавить новый перевал',
-    description='Принимает данные перевала и пользователя, сохраняет в базе. Устанавливает статус new.'
+    description='Создаёт перевал и связанные объекты: координаты, пользователя, фото. Устанавливает статус "new".'
 )
 class SubmitDataView(APIView):
     def post(self, request, format=None):
         serializer = PerevalSerializer(data=request.data)
+
         if serializer.is_valid():
-            pereval = serializer.save()
-            return Response(PerevalSerializer(pereval).data, status=status.HTTP_201_CREATED)
+            validated_data = serializer.validated_data
+
+            user_data = validated_data.pop('user')
+            coords_data = validated_data.pop('coords')
+            images_data = validated_data.pop('images', [])
+
+            user = SubmitDataService.get_or_create_user(user_data)
+            coords = SubmitDataService.create_coords(coords_data)
+            pereval = SubmitDataService.create_pereval(validated_data, user, coords)
+            SubmitDataService.attach_images(pereval, images_data)
+
+            response_data = PerevalSerializer(pereval).data
+            return Response(response_data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
