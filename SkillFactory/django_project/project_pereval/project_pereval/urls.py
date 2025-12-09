532d3d3beba0007e8ea93c1f5e67a5981830054e
Dmitry-K-off <dimon1734371@gmail.com>
2025-12-09 22:41:26 +0500

    Обновление
    Обновление после сбоя и потери доступа к базе данных

--- SkillFactory/django_project/project_pereval/project_pereval/urls.py
+++ SkillFactory/django_project/project_pereval/project_pereval/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/redoc/', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('pereval.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
