99434d8ed34d7ed784b54f5d24f491f59822a3ce
Dmitry-K-off <dimon1734371@gmail.com>
2025-12-12 22:24:52 +0500

    Модификация файла urls.py Проекта project_pereval.

--- SkillFactory/django_project/project_pereval/project_pereval/urls.py
+++ SkillFactory/django_project/project_pereval/project_pereval/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/api/redoc/', permanent=False), name='home'),
     path('admin/', admin.site.urls),
-    path('api/', include('pereval.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/', include('pereval.urls')),
+    path('', RedirectView.as_view(url='/api/redoc/', permanent=False), name='home'),
 ]
